82de9c6d0d8280cce93b06bf5f35991f9cc9c19d
ananyad5 <adandi@terpmail.umd.edu>
2025-04-05 12:31:29 -0400

    blessed version

--- game-of-life.py
+++ game-of-life.py
@@ -1,8 +1,27 @@
 from blessed import Terminal
-
+import time
+import os
+import random 
 
 term = Terminal()
 
+ALIVE = '*'
+DEAD = '.'
 
-with term.location(0, term.height-1):
-    print("this is"+ term.underline("underlined"))
+rows = 20
+cols = 20
+
+# creating and drawing a grid with * as alive cells and . as dead
+def initial_grid():
+    return [[random.chocie[0,1] for _ in range(cols)] for _ in range(rows)]
+
+
+def draw_grid(grid):
+    with term.location(0, 0):
+        for row in grid:
+            line = ''.join(term.green('*') if cell else term.red('.') for cell in row)
+            print(line)
+
+
+def apply_rules(grid, row, col):
+    
